f9795d27d805232f2e60bf85f95b578529ec4432
Neel1292 <prajapatineel122002@gmail.com>
2024-02-23 14:53:58 +0530

    Code Formatting

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,31 +1,30 @@
-// $(document).DOM(function () {
-  const timeSheets = JSON.parse(localStorage.getItem("timeSheet"));
-  var displayData = document.querySelector(".table-body");
-  var displayTotalHours = document.getElementById("workingHours");
-  const viewForm = document.querySelector(".data-view");
-  const tableView = document.querySelector(".table-conatienr");
-  const searchInput = document.querySelector("#search");
-  var totalWorkHours = 0;
+const timeSheets = JSON.parse(localStorage.getItem("timeSheet"));
+var displayData = document.querySelector(".table-body");
+var displayTotalHours = document.getElementById("workingHours");
+const viewForm = document.querySelector(".data-view");
+const tableView = document.querySelector(".table-conatienr");
+const searchInput = document.querySelector("#search");
+var totalWorkHours = 0;
 
-  let userEmail = sessionStorage.getItem("user");
+let userEmail = sessionStorage.getItem("user");
 
-  getDataView();
+getDataView();
 
-  searchInput.addEventListener("input", function (event) {
-    const searchDate = event.target.value; // Get the selected date from the Date input
-    const fromattedSearchDate = formatDate(searchDate); // Function to format date to 'dd-mm-yyyy' format
-    displayData.innerHTML = ""; // Clear previous results
-    totalWorkHours = 0;
+searchInput.addEventListener("input", function (event) {
+  const searchDate = event.target.value; // Get the selected date from the Date input
+  const fromattedSearchDate = formatDate(searchDate); // Function to format date to 'dd-mm-yyyy' format
+  displayData.innerHTML = ""; // Clear previous results
+  totalWorkHours = 0;
 
-    const filteredTimeSheets = timeSheets.filter((sheet) => {
-      console.log(sheet.date === sheet.date);
-      return sheet.email === userEmail && sheet.date === fromattedSearchDate;
-    });
-    if (filteredTimeSheets.length > 0) {
-      filteredTimeSheets.forEach((sheet, index) => {
-        totalWorkHours += sheet.totalWork;
+  const filteredTimeSheets = timeSheets.filter((sheet) => {
+    console.log(sheet.date === sheet.date);
+    return sheet.email === userEmail && sheet.date === fromattedSearchDate;
+  });
+  if (filteredTimeSheets.length > 0) {
+    filteredTimeSheets.forEach((sheet, index) => {
+      totalWorkHours += sheet.totalWork;
 
-        displayData.innerHTML += `
+      displayData.innerHTML += `
         <tr>
             <td>${sheet.date}</td>
             <td>${sheet.checkIn}</td>
@@ -33,42 +32,42 @@
             <td>${msToHms(sheet.totalWork)}</td>
             <td><i class="fa-solid fa-calendar-minus" onclick="deleteUser()" data-index="${index}"></i></td>
         </tr>`;
-      });
-      displayTotalHours.innerText = `${msToHms(totalWorkHours)}`;
-    } else {
-      // If no records found, display "No Record" in the <td>
-      displayData.innerHTML = `
+    });
+    displayTotalHours.innerText = `${msToHms(totalWorkHours)}`;
+  } else {
+    // If no records found, display "No Record" in the <td>
+    displayData.innerHTML = `
       <tr>
           <td colspan="4">No Record</td>
       </tr>`;
-      displayTotalHours.innerText = ""; // Clear total hours display
-    }
-  });
-
-  // Function to format date to 'dd-mm-yyyy' format
-  function formatDate(date) {
-    const [year, month, day] = date.split("-");
-    return `${day}-${month}-${year}`;
+    displayTotalHours.innerText = ""; // Clear total hours display
   }
+});
 
-  function getDataView() {
-    totalWorkHours = 0;
-    
-    if (timeSheets && timeSheets.length > 0) {
-      const timeSheetsSortedByDate = timeSheets.slice().sort((a, b) => {
-        const dateA = new Date(`${a.date} ${a.checkIn}`);
-        const dateB = new Date(`${b.date} ${b.checkIn}`);
-        return dateA.getTime() - dateB.getTime();
-      });
+// Function to format date to 'dd-mm-yyyy' format
+function formatDate(date) {
+  const [year, month, day] = date.split("-");
+  return `${day}-${month}-${year}`;
+}
 
-      // displayTotalHours.innerText = ''
-      timeSheetsSortedByDate.forEach((sheet, index) => {
-        if (sheet.email === userEmail) {
-          // const hours = Math.floor(sheet.totalWork / (1000 * 60 * 60));
-          // const minutes = Math.floor((sheet.totalWork % (1000 * 60 * 60)) / (1000 * 60));
-          totalWorkHours += sheet.totalWork;
+function getDataView() {
+  totalWorkHours = 0;
 
-          displayData.innerHTML += `
+  if (timeSheets && timeSheets.length > 0) {
+    const timeSheetsSortedByDate = timeSheets.slice().sort((a, b) => {
+      const dateA = new Date(`${a.date} ${a.checkIn}`);
+      const dateB = new Date(`${b.date} ${b.checkIn}`);
+      return dateA.getTime() - dateB.getTime();
+    });
+
+    // displayTotalHours.innerText = ''
+    timeSheetsSortedByDate.forEach((sheet, index) => {
+      if (sheet.email === userEmail) {
+        // const hours = Math.floor(sheet.totalWork / (1000 * 60 * 60));
+        // const minutes = Math.floor((sheet.totalWork % (1000 * 60 * 60)) / (1000 * 60));
+        totalWorkHours += sheet.totalWork;
+
+        displayData.innerHTML += `
           <tr>
             <td>${sheet.date}</td>
             <td>${sheet.checkIn}</td>
@@ -76,43 +75,35 @@
             <td>${msToHms(sheet.totalWork)}</td>
             <td><i class="fa-solid fa-calendar-minus" onclick="deleteUser(${index})"></i></td>
           </tr>`;
-        }
-        // <td>${hours}:${minutes < 10 ? '0' : ''}${minutes}</td>
-      });
-      displayTotalHours.innerText = `${msToHms(totalWorkHours)}`;
-
-
-    } else {
-      tableView.innerHTML = `<h2>No Data Available...</h2><br/>`;
-      viewForm.classList.remove("data_table");
-    }
+      }
+      // <td>${hours}:${minutes < 10 ? '0' : ''}${minutes}</td>
+    });
+    displayTotalHours.innerText = `${msToHms(totalWorkHours)}`;
+  } else {
+    tableView.innerHTML = `<h2>No Data Available...</h2><br/>`;
+    viewForm.classList.remove("data_table");
   }
+}
 
-  function msToHms(milliseconds) {
-    const totalSeconds = milliseconds / 1000;
-    const hours = Math.floor(totalSeconds / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = Math.floor(totalSeconds % 60);
+function msToHms(milliseconds) {
+  const totalSeconds = milliseconds / 1000;
+  const hours = Math.floor(totalSeconds / 3600);
+  const minutes = Math.floor((totalSeconds % 3600) / 60);
+  const seconds = Math.floor(totalSeconds % 60);
 
-    const paddedMinutes = String(minutes).padStart(2, "0");
-    const paddedSeconds = String(seconds).padStart(2, "0");
+  const paddedMinutes = String(minutes).padStart(2, "0");
+  const paddedSeconds = String(seconds).padStart(2, "0");
 
-    return `${String(hours).padStart(
-      2,
-      "0"
-    )}:${paddedMinutes}:${paddedSeconds}`;
-  }
+  return `${String(hours).padStart(2, "0")}:${paddedMinutes}:${paddedSeconds}`;
+}
 
-  
-// });
 function deleteUser(index) {
-  // debugger
   // Display confirmation block
   if (confirm("Are you sure you want to delete this user's data?")) {
     // If user confirms, perform the deletion
     timeSheets.splice(index, 1);
-    localStorage.setItem('timeSheet', JSON.stringify(timeSheets));
-    displayData.innerHTML = '';
+    localStorage.setItem("timeSheet", JSON.stringify(timeSheets));
+    displayData.innerHTML = "";
     getDataView();
   }
 }
